e96e0283c4e4f2051c43aab51ed0e6835e1a4915
Duncan <ducanishah@gmail.com>
2019-06-06 22:24:01 -0700

    goblin wanders, but out-of-bounds errors itself by not checking before moving

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ document.addEventListener("keydown", inputHandler)
 
 //constructor parameters: x value, y value, display priority, name, symbol
 class Actor {
-    constructor(xSet, ySet,dispPrior=0,myName,mySymbol) {
-        this.name=myName;
-        this.mapSymbol=mySymbol;
+    constructor(xSet, ySet, dispPrior = 0, myName, mySymbol) {
+        this.name = myName;
+        this.mapSymbol = mySymbol;
         this.location = { x: xSet, y: ySet };
-        this.displayPriority=dispPrior;
+        this.displayPriority = dispPrior;
         myActorHolder.aliveActors.push(this);
         actorPlace(this, xSet, ySet);
     }
 }
 class Player extends Actor {
-    constructor(xSet, ySet,dispPrior=2) {
-        super(xSet, ySet,dispPrior,"player","P")
-        this.mapSymbol = "P";
+    constructor(xSet, ySet, dispPrior = 2) {
+        super(xSet, ySet, dispPrior, "player", "P")
     }
     move(direction) {
         switch (direction) {
@@ -39,21 +38,46 @@ class Player extends Actor {
     }
 }
 class Goblin extends Actor {
-    constructor(setX,setY,dispPrior=1){
-        super(setX,setY,dispPrior,"goblin","g");
-        this.mapSymbol="g";
-        actorPlace(this,setX,setY);
+    constructor(setX, setY, dispPrior = 1) {
+        super(setX, setY, dispPrior, "goblin", "g");
     }
-    update() {}
+    update() {
+        this.wander()
+    }
+    wander() {
+        let myDirection;
+        switch (Math.ceil(Math.random() * 4)) {
+            case (1):
+                //up
+                myDirection=[0,-1]
+                break;
+            case (2):
+                //down
+                myDirection=[0,1]
+                break;
+            case (3):
+                //left
+                myDirection=[1,0]
+                break;
+            case (4):
+                //right
+                myDirection=[-1,0]
+                break;
+        }
+        this.move(myDirection)
+    }
+    move(direction){
+        actorPlace(this,this.location.x+direction[0],this.location.y+direction[1]);
+    }
 }
-
+//Call update on this to have it call update on all the fools
 class ActorHolder {
     constructor() {
-        this.aliveActors=[];
+        this.aliveActors = [];
     }
-    update(){
-        for(let i=0; i<this.aliveActors.length;i++){
-            if(this.aliveActors[i].update){this.aliveActors[i].update();}
+    update() {
+        for (let i = 0; i < this.aliveActors.length; i++) {
+            if (this.aliveActors[i].update) { this.aliveActors[i].update(); }
         }
     }
 }
@@ -65,7 +89,7 @@ class worldLocation {
     }
 }
 
-var myActorHolder=new ActorHolder();
+var myActorHolder = new ActorHolder();
 var playerChar;
 var worldMap;
 
@@ -73,51 +97,28 @@ function main() {
     // console.log(new Person().alive);
     initializeWorldMap();
     playerChar = new Player(8, 8);
-    new Goblin (8,8);
-    updateWorldMap();
+    new Goblin(8, 8);
+    updateWorldTable();
 }
 
-function updateWorldMap(){
-    if(document.getElementById("outerWrapper").children[0]) {
+function updateWorldTable() {
+    if (document.getElementById("outerWrapper").children[0]) {
         document.getElementById("outerWrapper").children[0].remove();
     }
     document.getElementById("outerWrapper").append(createWorldTable());
 }
-
+//call this to move an actor from its present spot (or non-spot) to another spot
 function actorPlace(actor, x, y) {
-    let actorPresentLocation=worldMap[actor.location.x][actor.location.y]
-    if(x<0 || x>worldMap.length-1 || y<0 || y>worldMap.length-1){
+    let actorPresentLocation = worldMap[actor.location.x][actor.location.y]
+    if (x < 0 || x > worldMap.length - 1 || y < 0 || y > worldMap.length - 1) {
         console.log("Out of bounds error");
         return false;
     }
-    if(actorPresentLocation.presentActors.indexOf(actor)!=-1){
-        actorPresentLocation.presentActors.splice(actorPresentLocation.presentActors.indexOf(actor),1);
+    if (actorPresentLocation.presentActors.indexOf(actor) != -1) {
+        actorPresentLocation.presentActors.splice(actorPresentLocation.presentActors.indexOf(actor), 1);
     }
     worldMap[x][y].presentActors.push(actor);
     actor.location = { x, y }
-}
-
-//THERE ARE UPDATE CALLS IN HERE
-function inputHandler(e) {
-    // console.log(e.code);
-    switch (e.code) {
-        case "ArrowUp":
-            playerChar.move("up")
-            break;
-        case "ArrowDown":
-            playerChar.move("down")
-            break;
-        case "ArrowLeft":
-            playerChar.move("left")
-            break;
-        case "ArrowRight":
-            playerChar.move("right")
-            break;
-        default:
-            break;
-    }
-    myActorHolder.update();
-    updateWorldMap();
 }
 
 function initializeWorldMap() {
@@ -144,8 +145,8 @@ function createWorldTable() {
 
             if (item.presentActors.length) {
                 //Sort by display priority, with highest number being first
-                item.presentActors.sort(function(a,b){
-                    if (a.displayPriority<b.displayPriority){
+                item.presentActors.sort(function (a, b) {
+                    if (a.displayPriority < b.displayPriority) {
                         return true;
                     } else {
                         return false;
@@ -162,6 +163,28 @@ function createWorldTable() {
 }
 
 
+//THERE ARE UPDATE CALLS IN HERE
+function inputHandler(e) {
+    // console.log(e.code);
+    switch (e.code) {
+        case "ArrowUp":
+            playerChar.move("up")
+            break;
+        case "ArrowDown":
+            playerChar.move("down")
+            break;
+        case "ArrowLeft":
+            playerChar.move("left")
+            break;
+        case "ArrowRight":
+            playerChar.move("right")
+            break;
+        default:
+            break;
+    }
+    myActorHolder.update();
+    updateWorldTable();
+}
 
 
 
