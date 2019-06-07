0185931dc99e42a7e07b1b1bcd46dd1b2b4a6065
ducanishah <ducanishah@gmailcom>
2019-06-07 11:42:59 -0700

    added shared-space-checking and postUpdate probably other stuff made better

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ document.addEventListener("DOMContentLoaded", main);
 document.addEventListener("keydown", inputHandler)
 //TODOS IN COMMENTS
 
+class worldLocation {
+    constructor(setX,setY) {
+        this.presentActors = []
+        this.x=setX;
+        this.y=setY;
+    }
+}
 //constructor parameters: x value, y value, display priority, name, symbol
 class Actor {
     constructor(xSet, ySet, dispPrior = 0, myName, mySymbol) {
         this.name = myName;
         this.mapSymbol = mySymbol;
-        this.location = { x: xSet, y: ySet };
         this.displayPriority = dispPrior;
+        this.location;
         myActorHolder.aliveActors.push(this);
         actorPlace(this, xSet, ySet);
     }
 }
 class Player extends Actor {
-    constructor(xSet, ySet, dispPrior = 2) {
-        super(xSet, ySet, dispPrior, "player", "P")
+    constructor(setX, setY, dispPrior = 2) {
+        super(setX, setY, dispPrior, "player", "P")
     }
     move(direction) {
         switch (direction) {
@@ -44,6 +51,11 @@ class Goblin extends Actor {
     update() {
         this.wander()
     }
+    postUpdate(){
+        if(sharesLocation(this,Player)){
+            console.log("Eek! A player!")
+        }
+    }
     wander() {
         let myDirection;
         switch (Math.ceil(Math.random() * 4)) {
@@ -70,7 +82,13 @@ class Goblin extends Actor {
         actorPlace(this,this.location.x+direction[0],this.location.y+direction[1]);
     }
 }
-//Call update on this to have it call update on all the fools
+class Tree extends Actor {
+    constructor (setX,setY,dispPrior=0){
+        super(setX,setY,dispPrior,"tree","T")
+    }
+}
+//Call update on this to have it call update AND postUpdate on all the actors
+//put movement and stuff in update, checking for shared squares in postUpdate
 class ActorHolder {
     constructor() {
         this.aliveActors = [];
@@ -79,26 +97,30 @@ class ActorHolder {
         for (let i = 0; i < this.aliveActors.length; i++) {
             if (this.aliveActors[i].update) { this.aliveActors[i].update(); }
         }
-    }
-}
-
-
-class worldLocation {
-    constructor() {
-        this.presentActors = []
+        for (let i = 0; i < this.aliveActors.length; i++) {
+            if (this.aliveActors[i].postUpdate) { this.aliveActors[i].postUpdate(); }
+        }
     }
 }
 
 var myActorHolder = new ActorHolder();
 var playerChar;
 var worldMap;
+var worldMapLength=16;
 
 function main() {
     // console.log(new Person().alive);
     initializeWorldMap();
-    playerChar = new Player(8, 8);
-    new Goblin(8, 8);
+    spawnInitialActors();
     updateWorldTable();
+    // console.log(playerChar instanceof Player)
+}
+
+function spawnInitialActors() {
+    let place=generateRandomLocation();
+    playerChar = new Player(8, 8);
+    new Goblin(...place);
+    new Tree(...place);
 }
 
 function updateWorldTable() {
@@ -110,10 +132,10 @@ function updateWorldTable() {
 
 function initializeWorldMap() {
     worldMap = [];
-    for (let i = 0; i < 16; i++) {
-        worldMap[i] = new Array(16);
+    for (let i = 0; i < worldMapLength; i++) {
+        worldMap[i] = new Array(worldMapLength);
         for (let ind = 0; ind < worldMap[i].length; ind++) {
-            worldMap[i][ind] = new worldLocation;
+            worldMap[i][ind] = new worldLocation(i,ind);
         }
     }
 
@@ -121,7 +143,7 @@ function initializeWorldMap() {
 
 function createWorldTable() {
     let myTable = document.createElement("table");
-    let myRows = new Array(16);
+    let myRows = new Array(worldMap.length);
     for (let i = 0; i < myRows.length; i++) {
         myRows[i] = document.createElement("tr")
     }
@@ -129,18 +151,15 @@ function createWorldTable() {
     worldMap.forEach((subArray, subArrayIndex) => {
         subArray.forEach((item, itemIndex) => {
             let tempData = document.createElement("td");
-
             if (item.presentActors.length) {
-                //Sort by display priority, with highest number being first
-                item.presentActors.sort(function (a, b) {
-                    if (a.displayPriority < b.displayPriority) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                });
+                let topItem=item.presentActors[0];
+                for(let i=1;i<item.presentActors.length;i++){
+                    if(item.presentActors[i].displayPriority>topItem.displayPriority){
+                        topItem=item.presentActors[i];
+                    }//TODO CONTINUE
+                }
                 //display symbol of whichever is first
-                tempData.innerHTML = item.presentActors[0].mapSymbol;
+                tempData.innerHTML = topItem.mapSymbol;
             }
             myRows[itemIndex].append(tempData);
         });
@@ -150,16 +169,15 @@ function createWorldTable() {
 }
 //call this to move an actor from its present spot (or non-spot) to another spot
 function actorPlace(actor, x, y) {
-    let actorPresentLocation = worldMap[actor.location.x][actor.location.y]
     if (x < 0 || x > worldMap.length - 1 || y < 0 || y > worldMap.length - 1) {
         console.log("Out of bounds error");
         return false;
     }
-    if (actorPresentLocation.presentActors.indexOf(actor) != -1) {
-        actorPresentLocation.presentActors.splice(actorPresentLocation.presentActors.indexOf(actor), 1);
+    if (actor.location) {
+        actor.location.presentActors.splice(actor.location.presentActors.indexOf(actor), 1);
     }
     worldMap[x][y].presentActors.push(actor);
-    actor.location = { x, y }
+    actor.location = worldMap[x][y];
 }
 //THERE ARE UPDATE CALLS IN HERE
 function inputHandler(e) {
@@ -184,7 +202,34 @@ function inputHandler(e) {
     updateWorldTable();
 }
 
-
-
+//accepts an array input with minx, maxx, miny, maxy
+//returns in form [x,y]
+function generateRandomLocation(arr){
+    let x;
+    let y;
+    if(arr){
+        if(arr[0]<0||arr[2]<0||arr[1]>worldMap.length-1||arr[3]>worldMap.length-1){
+            console.log("These bounds are out of this world!");
+        }
+        if(arr[0]>arr[1]||arr[2]>arr[3]){
+            console.log("min needs to be smaller than max")
+        }
+        x=Math.floor(Math.random()*(arr[1]-arr[0]))+arr[0];
+        y=Math.floor(Math.random()*(arr[3]-arr[2]))+arr[2];
+    } else{
+        x= Math.floor(Math.random()*worldMap.length);
+        y=Math.floor(Math.random()*worldMap.length);
+    }
+    return [x,y];
+}
+//checks if given actor shares its location with any actor of a given class, excluding itself
+function sharesLocation(actor,typeToLookFor){
+    for(i=0;i<actor.location.presentActors.length;i++){
+        if(actor.location.presentActors[i] instanceof typeToLookFor && actor.location.presentActors[i]!=actor){
+            return true;
+        }
+    }
+    return false;
+}
 
 
